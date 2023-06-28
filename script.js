442a4c372a39b3fe14328a2dc8bedddec686df13
Nsalex18 <nicolassimoes861@gmail.com>
2023-06-28 18:31:25 -0300

    terceiro-commit

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const convertButton = document.querySelector("#convert-button");
 
 function convertCurrency(){
-    console.log("funcionou");
+
+    const inputCurrencyValue = document.getElementById("input-currency").value;
+    
+    const dolartoday = 4.90
+
+    const covertedValue = inputCurrencyValue / dolartoday
+
+    console.log(covertedValue)
 }
 
 convertButton.addEventListener('click', convertCurrency);
