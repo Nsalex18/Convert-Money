870ded8817e4c53233a946855ac6fbcdb49076ba
Nsalex18 <nicolassimoes861@gmail.com>
2023-06-28 20:20:43 -0300

    add.valores-real/dol

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const convertButton = document.querySelector("#convert-button");
 
-function convertCurrency(){
-
+function convertCurrency() {
     const inputCurrencyValue = document.getElementById("input-currency").value;
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert"); // Valor real
+    const currencyValueConverted = document.querySelector(".currency-value"); // Outros Valores
     
-    const dolartoday = 4.90
+    const dolartoday = 4.90;
 
-    const covertedValue = inputCurrencyValue / dolartoday
+    const convertedValue = inputCurrencyValue / dolartoday;
 
-    console.log(covertedValue)
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency:"BRL"
+    }).format(inputCurrencyValue);
+
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue);
+    
+
+    console.log(convertedValue);
 }
 
 convertButton.addEventListener('click', convertCurrency);
