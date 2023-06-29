b9683d3c776dbab5c97683218e1bb7d42cbf941a
Nsalex18 <nicolassimoes861@gmail.com>
2023-06-28 22:50:01 -0300

    add.euro

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 const convertButton = document.querySelector("#convert-button");
+const currencySelect = document.querySelector(".currency-select")
 
 function convertCurrency() {
     const inputCurrencyValue = document.getElementById("input-currency").value;
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert"); // Valor real
     const currencyValueConverted = document.querySelector(".currency-value"); // Outros Valores
     
+    console.log(currencySelect.value)
     const dolartoday = 4.90;
+    const eurotoday = 5.30;
 
-    const convertedValue = inputCurrencyValue / dolartoday;
+    if(currencySelect.value == "Dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolartoday);
+    }
+    
+    if(currencySelect.value == "Euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / eurotoday);
+        
+    }
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency:"BRL"
     }).format(inputCurrencyValue);
 
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(convertedValue);
     
-
-    console.log(convertedValue);
+    
 }
 
 convertButton.addEventListener('click', convertCurrency);
