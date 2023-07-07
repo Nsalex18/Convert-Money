db70d267384481cc032b168849708d282cd9bcac
cajudeleite <nathandebaz266@gmail.com>
2023-07-07 19:36:30 -0300

    Versao caju

--- script.js
+++ script.js
@@ -1,15 +1,57 @@
+/**
+ * Object para cada currency tendo sua taxa de conversao, seu locales,
+ * o nome da currency, o nome completo e o caminho da imagem
+ */
+const currencyLibrary = {
+  Real: {
+    conversionRate: 1,
+    locales: "pt-BR",
+    currency: "BRL",
+    name: "Real Brasileiro",
+    imagePath: "./assets/Real.png",
+  },
+  Dolar: {
+    conversionRate: 4.9,
+    locales: "en-US",
+    currency: "USD",
+    name: "Dólar Americano",
+    imagePath: "./assets/Dolar.png",
+  },
+  Euro: {
+    conversionRate: 5.3,
+    locales: "de-DE",
+    currency: "EUR",
+    name: "Euro",
+    imagePath: "./assets/Euro.png",
+  },
+  Libra: {
+    conversionRate: 6.15,
+    locales: "en-UK",
+    currency: "GBP",
+    name: "Libra",
+    imagePath: "./assets/Libra.png",
+  },
+  Bitcoin: {
+    conversionRate: 149333.95,
+    locales: "en-UK",
+    currency: "BTC",
+    name: "Bitcoin",
+    imagePath: "./assets/Bitcoin.png",
+  },
+  Iene: {
+    conversionRate: 0.033,
+    locales: "ja-JP",
+    currency: "JPY",
+    name: "Yen",
+    imagePath: "./assets/Japan.jpg",
+  },
+};
+
 const convertButton = document.querySelector("#convert-button");
 const currencySelect = document.querySelector(".currency-select");
 const currencySelectToConvert = document.querySelector(
   ".currency-select-to-convert"
 );
-
-const dolartoday = 4.9;
-const eurotoday = 5.3;
-const libratoday = 6.15;
-const bitcointoday = 149333.95;
-const ienetoday = 0.033;
-const realtoday = 1;
 
 /**
  * Function que retorna o valor a ser convertido na moeda selecionada convertido em real
@@ -20,25 +62,7 @@ const realtoday = 1;
 function currencyToReal(value) {
   const currency = currencySelectToConvert.value;
 
-  switch (currency) {
-    case "Dolar":
-      return value * dolartoday;
-
-    case "Euro":
-      return value * eurotoday;
-
-    case "Libra":
-      return value * libratoday;
-
-    case "Bitcoin":
-      return value * bitcointoday;
-
-    case "Iene":
-      return value * ienetoday;
-
-    default:
-      return value;
-  }
+  return currencyLibrary[currency].conversionRate * value;
 }
 
 /**
@@ -51,117 +75,27 @@ function convertCurrency() {
   ); // Valor
   const currencyValueConverted = document.querySelector(".currency-value"); // Outros Valores
 
+  const currency = currencySelect.value;
+  const currencyToConvert = currencySelectToConvert.value;
   const convertedCurrency = currencyToReal(inputCurrencyValue); // Valor da moeda a ser convertida em real
 
-  switch (currencySelect.value) {
-    case "Dolar":
-      // se o select estiver selecionado o valor de dolar, entra aqui
-      currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(convertedCurrency / dolartoday);
-
-      break;
-
-    case "Euro":
-      // se o select estiver selecionado o valor de euro, entra aqui
-      currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
-        style: "currency",
-        currency: "EUR",
-      }).format(convertedCurrency / eurotoday);
-
-      break;
-
-    case "Libra":
-      currencyValueConverted.innerHTML = new Intl.NumberFormat("en-UK", {
-        style: "currency",
-        currency: "GBP",
-      }).format(convertedCurrency / libratoday);
-
-      break;
-
-    case "Bitcoin":
-      currencyValueConverted.innerHTML = new Intl.NumberFormat("en-UK", {
-        style: "currency",
-        currency: "BTC",
-      }).format(convertedCurrency / bitcointoday);
-
-      break;
-
-    case "Iene":
-      currencyValueConverted.innerHTML = new Intl.NumberFormat("ja-JP", {
-        style: "currency",
-        currency: "JPY",
-      }).format(convertedCurrency / ienetoday);
-
-      break;
-
-    case "Real":
-      currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL",
-      }).format(convertedCurrency / realtoday);
-
-      break;
-
-    default:
-      break;
-  }
+  currencyValueConverted.innerHTML = new Intl.NumberFormat(
+    currencyLibrary[currency].locales,
+    {
+      style: "currency",
+      currency: currencyLibrary[currency].currency,
+    }
+  ).format(convertedCurrency / currencyLibrary[currency].conversionRate);
 
   // Alterando os cifrão das moedas de cima
 
-  switch (currencySelectToConvert.value) {
-    case "Real":
-      currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-        style: "currency",
-        currency: "BRL",
-      }).format(inputCurrencyValue);
-
-      break;
-
-    case "Dolar":
-      currencyValueToConvert.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-      }).format(inputCurrencyValue);
-
-      break;
-
-    case "Euro":
-      currencyValueToConvert.innerHTML = new Intl.NumberFormat("de-DE", {
-        style: "currency",
-        currency: "EUR",
-      }).format(inputCurrencyValue);
-
-      break;
-
-    case "Libra":
-      currencyValueToConvert.innerHTML = new Intl.NumberFormat("en-UK", {
-        style: "currency",
-        currency: "GBP",
-      }).format(inputCurrencyValue);
-
-      break;
-
-    case "Bitcoin":
-      currencyValueToConvert.innerHTML = new Intl.NumberFormat("en-UK", {
-        style: "currency",
-        currency: "BTC",
-      }).format(inputCurrencyValue);
-
-      break;
-
-    case "Iene":
-      currencyValueToConvert.innerHTML = new Intl.NumberFormat("ja-JP", {
-        style: "currency",
-        currency: "JPY",
-      }).format(inputCurrencyValue);
-
-      break;
-
-    default:
-      break;
-  }
+  currencyValueToConvert.innerHTML = new Intl.NumberFormat(
+    currencyLibrary[currencyToConvert].locales,
+    {
+      style: "currency",
+      currency: currencyLibrary[currencyToConvert].currency,
+    }
+  ).format(inputCurrencyValue);
 }
 
 /**
@@ -172,47 +106,12 @@ function convertCurrency() {
  * @param {HTMLImageElement} currencyImage HTMLImageElement da imagem da currency
  */
 function changeCurrency(currency, currencyName, currencyImage) {
-  switch (currency) {
-    case "Dolar":
-      currencyName.innerHTML = "Dólar Americano";
-      currencyImage.src = "./assets/Dolar.png";
+  currencyName.innerHTML = currencyLibrary[currency].name;
+  currencyImage.src = currencyLibrary[currency].imagePath;
 
-      break;
-
-    case "Euro":
-      currencyName.innerHTML = "Euro";
-      currencyImage.src = "./assets/Euro.png";
-
-      break;
-
-    case "Libra":
-      currencyName.innerHTML = "Libra";
-      currencyImage.src = "./assets/libra.png";
-
-      break;
-
-    case "Bitcoin":
-      currencyName.innerHTML = "Bitcoin";
-      currencyImage.src = "./assets/bitcoin.png";
-
-      break;
-
-    case "Iene":
-      currencyName.innerHTML = "Yen";
-      currencyImage.src = "./assets/japan.jpg";
-      currencyImage.width = 44;
-      currencyImage.height = 44;
-
-      break;
-
-    case "Real":
-      currencyName.innerHTML = "Real Brasileiro";
-      currencyImage.src = "./assets/Real.png";
-
-      break;
-
-    default:
-      break;
+  if (currency === "Iene") {
+    currencyImage.width = 44;
+    currencyImage.height = 44;
   }
 
   convertCurrency();
